68ecbaf58b2bd414ae0bbdaf0f59cc56ec5994e8
rojasespinolamario <rojasespinolamario@gmail.com>
2026-05-23 16:53:15 -0500

    feat: implemente la funcionalidad de la clase Cart

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -1,4 +1,30 @@
 package model;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cart {
+
+    private List<Product> products;
+
+    public Cart() {
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+
+        return total;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
 }
