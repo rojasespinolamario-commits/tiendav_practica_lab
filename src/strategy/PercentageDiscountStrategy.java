3db72e79a4c4c65a722d2349d0407fac3c1ee399
rojasespinolamario <rojasespinolamario@gmail.com>
2026-05-23 17:00:56 -0500

    feat: implemente la clase PercentageDiscountStrategy

--- src/strategy/PercentageDiscountStrategy.java
+++ src/strategy/PercentageDiscountStrategy.java
@@ -1,4 +1,15 @@
 package strategy;
 
-public class PercentageDiscountStrategy {
+public class PercentageDiscountStrategy implements DiscountStrategy {
+
+    private double percentage;
+
+    public PercentageDiscountStrategy(double percentage) {
+        this.percentage = percentage;
+    }
+
+    @Override
+    public double applyDiscount(double total) {
+        return total - (total * percentage / 100);
+    }
 }
