26a92a95e17dccdf91404fea0ea1438806520568
rojasespinolamario <rojasespinolamario@gmail.com>
2026-05-23 17:02:56 -0500

    feat: implemente la clase FixedAmountDiscountStrategy

--- src/strategy/FixedAmountDiscountStrategy.java
+++ src/strategy/FixedAmountDiscountStrategy.java
@@ -1,4 +1,15 @@
 package strategy;
 
-public class FixedAmountDiscountStrategy {
+public class FixedAmountDiscountStrategy  implements DiscountStrategy {
+
+    private double amount;
+
+    public FixedAmountDiscountStrategy(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public double applyDiscount(double total) {
+        return total - amount;
+    }
 }
