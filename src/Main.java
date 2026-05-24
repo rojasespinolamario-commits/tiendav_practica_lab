a7f3fbe3a4b4c4b89eae68f4582c23665e8ba671
rojasespinolamario <rojasespinolamario@gmail.com>
2026-05-23 19:00:12 -0500

    refactor: mejore la presentacion en el main class

--- src/Main.java
+++ src/Main.java
@@ -12,15 +12,31 @@ import strategy.PercentageDiscountStrategy;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("===== TIENDA VIRTUAL =====");
+        System.out.println("--------------------------");
+
         Product laptop= new Product("Laptop", 3500);
         Product mouse= new Product("Mouse", 100);
+        Product teclado= new Product("Teclado", 120.0);
 
         Cart cart = new Cart();
         cart.addProduct(laptop);
         cart.addProduct(mouse);
+        cart.addProduct(teclado);
+
+        System.out.println("\nPRODUCTOS EN CARRITO:");
+
+        for (Product product: cart.getProducts()) {
+            System.out.println(product.getName() + " - S/ " + product.getPrice());
+        }
+
+        System.out.println("--------------------------");
 
         OrderService orderService = new OrderService();
+
         orderService.setDiscountStrategy(new PercentageDiscountStrategy(10));
+
+        System.out.println("Descuento aplicado: 10%");
 
         ExternalPayPalService payPalService = new ExternalPayPalService();
 
@@ -34,6 +50,10 @@ public class Main {
 
         orderService.addObserver(new AdminNotificationObserver());
 
+        System.out.println("\n----- PROCESANDO COMPRA -----");
+
         orderService.completeOrder(cart);
+
+        System.out.println("\n===== COMPRA FINALIZADA =====");
     }
 }
